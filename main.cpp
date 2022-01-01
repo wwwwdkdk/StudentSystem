1d5d6666165f1caea7ca6d9e0d774be4b3a71548
fg <63393116+wwwwdkdk@users.noreply.github.com>
2022-01-01 21:20:04 +0800

    init

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
